7c9729144f79d1cf5fc0e9c8457722a20eb00ecf
chyi13 <rongshuxiacy@gmail.com>
2019-04-19 22:35:09 +0800

    reset to new template

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,53 @@
 <template>
-    <div id="wrapper">
-        <!-- Navigation -->
-        <nav class="navbar navbar-light bg-light static-top">
+  <div id="wrapper">
+    <!-- Sidebar -->
+    <ul class="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion" id="accordionSidebar">
+        <a class="sidebar-brand d-flex align-items-center justify-content-center" href="index.html">
+        <div class="sidebar-brand-icon rotate-n-15">
+          <i class="fas fa-laugh-wink"></i>
+        </div>
+        <div class="sidebar-brand-text mx-3">SB Admin <sup>2</sup></div>
+      </a>
+    </ul>
+    <div id="content-wrapper" class="d-flex flex-column">
+      <!-- Main Content -->
+      <div id="content">
+        <!-- Topbar -->
+        <TopBar/>
+        <!-- Begin Page Content -->
+        <div class="container-fluid">
+          <!-- Masthead -->
+          <section class="features-icons bg-light">
+            <div class="container-fluid main-container">
+              <div class="row d-flex justify-content-center">
+                <div class="col-lg-8">
+                  <Run></Run>
+                </div>
+              </div>
+            </div>
+          </section>
+          <section class="features-icons bg-light">
+            <div class="container-fluid main-container">
+              <div class="row d-flex justify-content-center">
+                <div class="col-lg-8">
+                  <Edit></Edit>
+                </div>
+              </div>
+            </div>
+          </section>
+        </div>
+      </div>
+    </div>
+    <!-- Navigation -->
+    <!-- <nav class="navbar navbar-light bg-light static-top">
             <div class="container">
                 <a class="navbar-brand" href="#">Postet</a>
                 <a class="btn btn-primary" href="#">Sign In</a>
             </div>
-        </nav>
+    </nav>-->
 
-        <!-- Masthead -->
-        <header class="masthead text-white text-center">
+    <!-- Masthead -->
+    <!-- <header class="masthead text-white text-center">
             <div class="overlay"></div>
             <div class="container">
                 <div class="row">
@@ -19,57 +57,51 @@
                     <Run></Run>
                 </div>
             </div>
-        </header>
-        <!-- Icons Grid -->
-        <section class="features-icons bg-light">
-            <div class="container-fluid main-container">
-                <div class="row d-flex justify-content-center">
-                    <div class="col-lg-8">
-                        <Edit></Edit>
-                    </div>
-                </div>
-            </div>
-        </section>
-        <Result></Result>
-    </div>
+    </header>-->
+    <!-- Icons Grid -->
+
+    <Result></Result>
+  </div>
 </template>
 <script>
-    import Run from './components/Run.vue';
-    import Edit from './components/Edit.vue';
-    import Result from './components/Result.vue';
+import Run from "./components/Run.vue";
+import Edit from "./components/Edit.vue";
+import Result from "./components/Result.vue";
+import TopBar from "./components/TopBar.vue";
 
-    export default {
-        name: "home",
-        created() {
-            this.$store.dispatch('UPDATE_API_IDS');
-        },
-        data() {
-            return {
-                tabSelected: "preview",
-                showModal: false,
-            };
-        },
-        components: {
-            Edit,
-            Run,
-            Result,
-        },
-        methods: {
-            onTabClicked(tabName) {
-                this.tabSelected = tabName;
-            }
-        }
+export default {
+  name: "home",
+  created() {
+    this.$store.dispatch("UPDATE_API_IDS");
+  },
+  data() {
+    return {
+      tabSelected: "preview",
+      showModal: false
     };
+  },
+  components: {
+    Edit,
+    Run,
+    Result,
+    TopBar
+  },
+  methods: {
+    onTabClicked(tabName) {
+      this.tabSelected = tabName;
+    }
+  }
+};
 </script>
 <style>
-    #wrapper {
-        font-family: "Avenir", Helvetica, Arial, sans-serif;
-        -webkit-font-smoothing: antialiased;
-        -moz-osx-font-smoothing: grayscale;
-        text-align: left;
-        color: #2c3e50;
-    }
-    .modal-lg {
-        min-width: 65%;
-    }
+#wrapper {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: left;
+  color: #2c3e50;
+}
+.modal-lg {
+  min-width: 65%;
+}
 </style>
--- src/components/Run.vue
+++ src/components/Run.vue
@@ -1,48 +1,73 @@
 <template>
-  <div class="col-lg-12 col-md-9">
-    <div class="form-row">
-      <div class="col-12 col-md-11 mb-2 input-group">
-        <div class="input-group-prepend">
-          <span class="input-group-text">GET</span>
+  <div class="row">
+    <div class="col-lg-12">
+      <!-- Default Card Example -->
+      <div class="card mb-4">
+        <div class="card-header">
+           <h6 class="m-0 font-weight-bold text-primary">接口</h6>
         </div>
-        <div class="col-md-2">
-            <div class="form-group">
-                <select
-                    id="api_selector"
-                    data-live-search="true"
-                    class="selectpicker form-control"
-                >
-            </select>
+        <div class="card-body">
+          <div class="form-row">
+            <div class="col-md-11 mb-2 input-group">
+              <div class="input-group-prepend">
+                <span class="input-group-text">GET</span>
+              </div>
+              <select
+                id="run_selector"
+                data-live-search="true"
+                class="form-control form-control-lg"
+              >
+                <optgroup label="Climbing">
+                  <option value="pitons">Pitons</option>
+                  <option value="cams">Cams</option>
+                  <option value="nuts">Nuts</option>
+                  <option value="bolts">Bolts</option>
+                  <option value="stoppers">Stoppers</option>
+                  <option value="sling">Sling</option>
+                </optgroup>
+                <optgroup label="Skiing" disabled>
+                  <option value="skis">Skis</option>
+                  <option value="skins">Skins</option>
+                  <option value="poles">Poles</option>
+                </optgroup>
+              </select>
             </div>
+          </div>
         </div>
-        <div class="col-md-9">
-          <select
-            id="run_selector"
-            data-live-search="true"
-            data-width="75%"
-            class="form-control form-control-lg"
-          >
-            <optgroup label="Climbing">
-              <option value="pitons">Pitons</option>
-              <option value="cams">Cams</option>
-              <option value="nuts">Nuts</option>
-              <option value="bolts">Bolts</option>
-              <option value="stoppers">Stoppers</option>
-              <option value="sling">Sling</option>
-            </optgroup>
-            <optgroup label="Skiing" disabled>
-              <option value="skis">Skis</option>
-              <option value="skins">Skins</option>
-              <option value="poles">Poles</option>
-            </optgroup>
-          </select>
-        </div>
-      </div>
-      <div class="col-12 col-md-1">
-        <button class="btn btn-block btn-lg btn-primary" @click="send">Send!</button>
       </div>
     </div>
   </div>
+  <!-- <div class="col-lg-12 col-md-9">
+    <div class="form-row">
+      <div class="col-md-11 mb-2 input-group">
+        <div class="input-group-prepend">
+          <span class="input-group-text">GET</span>
+        </div>
+        <div class="input-group">
+          <div class="col-md-9">
+            <select id="run_selector" data-live-search="true" class="form-control form-control-lg">
+              <optgroup label="Climbing">
+                <option value="pitons">Pitons</option>
+                <option value="cams">Cams</option>
+                <option value="nuts">Nuts</option>
+                <option value="bolts">Bolts</option>
+                <option value="stoppers">Stoppers</option>
+                <option value="sling">Sling</option>
+              </optgroup>
+              <optgroup label="Skiing" disabled>
+                <option value="skis">Skis</option>
+                <option value="skins">Skins</option>
+                <option value="poles">Poles</option>
+              </optgroup>
+            </select>
+          </div>
+        </div>
+      </div>
+      <div class="col-md-1">
+        <button class="btn btn-block btn-lg btn-primary" @click="send">Send!</button>
+      </div>
+    </div>
+  </div>-->
 </template>
 
 <script>
